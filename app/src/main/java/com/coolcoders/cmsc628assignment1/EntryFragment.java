8001efeda37a0f72956bbc8f0aeaacba39ed0fee
Michael Davis <davism4@umbc.edu>
2015-03-22 10:46:25 -0400

    Switched right/left positions on displayed list (to match project instructions). Added logic for removing old entries. Shifted comments. TODO: Saving data to the SD card.

--- app/src/main/java/com/coolcoders/cmsc628assignment1/EntryFragment.java
+++ app/src/main/java/com/coolcoders/cmsc628assignment1/EntryFragment.java
@@ -30,6 +30,7 @@ public class EntryFragment extends ListFragment implements SensorEventListener {
     private final long lSensorInterval = 250; // 0.25 seconds
     private final long lWarningInterval = 120000; // 10 seconds
     private final long lRecordInterval = 7000; // 120 seconds
+    public final int iMaxRecordedEntries = 10; // remove stored data on 11th or older entries
 
     /** Track times between reading sensor data, showing warnings, and recording data */
     public static Handler _handler = null;
@@ -42,18 +43,17 @@ public class EntryFragment extends ListFragment implements SensorEventListener {
     /** Gets message from Warnings class depending on status of sensors detected. */
     public static int errorCode = Warnings.OK;
 
-    // So, the problem is that 1) the gyroscope has no idea which way is up, 2) the accelerometer
+   /* // So, the problem is that 1) the gyroscope has no idea which way is up, 2) the accelerometer
     // can feel acceleration due to gravity, but has no way to differentiate gravity vs. movement,
     // and 3) the user might be moving or have the phone in a weird position when they start using
     // the app. Proposed solution: Assume the user is not moving and is holding the device upright
     // when  they start the app. Then, set and maintain these values.
     private float xAxis;
     private float yAxis;
-    private float zAxis;
+    private float zAxis; */
 
     /** Store cumulative accelerator x,y,z data from the sensors */
     public static float[] _data_accelerometer;
-
 
     // TODO: populate list from MainActivity
     public void onCreate(Bundle savedInstanceState) {
@@ -64,9 +64,9 @@ public class EntryFragment extends ListFragment implements SensorEventListener {
             lRecordTimeStart = System.currentTimeMillis();
             lSensorTimeStart = System.currentTimeMillis() - 250; // offset .25s to avoid overlap
             lWarningTimeStart = System.currentTimeMillis() - 4000; // offset 4s to avoid overlap
-            // Attempt to create the sensor manager.
+            /* Attempt to create the sensor manager. */
             _sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
-            // First, check if a SensorManager was successfully created.
+            /* First, check if a SensorManager was successfully created. */
             _handler = new Handler();
         }
         if (_sensorManager==null) {
@@ -95,17 +95,14 @@ public class EntryFragment extends ListFragment implements SensorEventListener {
         lTimeEnd = System.currentTimeMillis();
         if (lTimeEnd - lSensorTimeStart >= lSensorInterval)
         {
-            // TODO: calculate sensor data
             // Accelerometer data
             final float xval = event.values[0];
             final float yval = event.values[1];
             final float zval = event.values[2];
-            // Accelerometer data
             if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
                 _handler.post(new Runnable(){
                     @Override
                     public void run() {
-                        // records the amount of movement, direction doesn't matter.
                         _data_accelerometer[0] += Math.abs(xval);
                         _data_accelerometer[1] += Math.abs(yval);
                         _data_accelerometer[2] += Math.abs(zval);
@@ -120,44 +117,44 @@ public class EntryFragment extends ListFragment implements SensorEventListener {
                     }});}
             lSensorTimeStart = lTimeEnd;
         }
-        // If enough time between warnings is done, display a new warning (if there are any).
+        /* If enough time between warnings is done, display a new warning (if there are any). */
         if (lTimeEnd - lWarningTimeStart >= lWarningInterval)
         {
             Toast.makeText(getActivity().getApplicationContext(), Warnings.Message(errorCode), Toast.LENGTH_SHORT).show();
-            // Update the time that the most recent warning was displayed.
+            /*Update the time that the most recent warning was displayed. */
             lWarningTimeStart = lTimeEnd;
         }
-        // If enough time has passed to save / calculate data, do that.
+        /* If enough time has passed to save / calculate data, do that. */
         if (lTimeEnd - lRecordTimeStart >= lRecordInterval)
         {
             String message = "<unknown>";
-            // put the data into a new item, then add to the displayed list
             if (_data_accelerometer[1] > _data_accelerometer[0] && _data_accelerometer[1] > _data_accelerometer[2])
             {
-
+                /* majority of force along y-axis implies device is oriented upright. */
                 if (_data_accelerometer[0]+_data_accelerometer[2]>20) {
-                    // a lot of movement on xz-plane generally implies walking
+                    /* a lot of movement on xz-plane generally implies walking */
                     message = "Walking";
                 } else {
-                    // otherwise implies vertical but non-moving
+                    /* otherwise assume non-moving */
                     message = "Sitting";
                 }
             }
             else {
+                /* majority of force along x- or z-axis implies device is oriented sideways. */
                 message = "Lying Down";
             }
+            /* put the data into a new item, then add to the displayed list */
             EntryItem item = new EntryItem(message, lRecordTimeStart, lTimeEnd);
             _items.add(item);
+            if (_items.size()>iMaxRecordedEntries) { /* remove entries that are too old */
+                _items.remove(0);
+            }
+            /* add the new entry, and then reset the sensor data */
             _adapter.notifyDataSetChanged();
-//            message = "x:"+_data_accelerometer[0]+",y:"+_data_accelerometer[1]+",z:"+_data_accelerometer[2];
-//            Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
             _data_accelerometer[0] = _data_accelerometer[1] = _data_accelerometer[2] = 0;
             lRecordTimeStart = lTimeEnd;
         }
     }
-
-
-    /** everything below this line basically works */
 
     @Override
     public void onResume() {
@@ -172,7 +169,6 @@ public class EntryFragment extends ListFragment implements SensorEventListener {
         _sensorManager.unregisterListener(this);
     }
 
-
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
@@ -183,6 +179,6 @@ public class EntryFragment extends ListFragment implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        // required for implementing SensorEventListener... nothing else here
     }
 }
